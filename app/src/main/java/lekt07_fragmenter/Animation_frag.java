a70af4f1d994237d23aab25e89fde185ab738bfb
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2019-09-13 12:18:04 +0200

    Forberedelse til næste semester

--- app/src/main/java/lekt07_fragmenter/Animation_frag.java
+++ app/src/main/java/lekt07_fragmenter/Animation_frag.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.Button;
 
@@ -94,5 +95,28 @@ public class Animation_frag extends Fragment {
     // Kunne også havde sat det deklarativt med android:animateLayoutChanges="true"
     rod.setLayoutTransition(new LayoutTransition());
     return rod;
+  }
+
+
+  static class ParrabelInterpolator implements Interpolator {
+    private final float mTension = 10;
+
+    /**
+     * Maps a value representing the elapsed fraction of an animation to a value that represents
+     * the interpolated fraction. This interpolated value is then multiplied by the change in
+     * value of an animation to derive the animated value at the current elapsed animation time.
+     *
+     * @param t A value between 0 and 1.0 indicating our current point
+     *          in the animation where 0 represents the start and 1.0 represents
+     *          the end
+     * @return The interpolation value. This value can be more than 1.0 for
+     * interpolators which overshoot their targets, or less than 0 for
+     * interpolators that undershoot their targets.
+     */
+    public float getInterpolation(float t) {
+      // bemærk, for t=1 skullle vi egentlig ende på 1,
+      // så det her er ikke nogen helt velfungerende interpolator, da den ender på 0!
+      return t * (1 - t) * mTension;
+    }
   }
 }
