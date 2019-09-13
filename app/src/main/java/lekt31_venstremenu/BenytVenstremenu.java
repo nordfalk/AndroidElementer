cfd59676eb13b1d4a9ce06334286f2f2f67e444a
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2019-09-13 12:37:01 +0200

    Forberedelse til næste semester

--- app/src/main/java/lekt31_venstremenu/BenytVenstremenu.java
+++ app/src/main/java/lekt31_venstremenu/BenytVenstremenu.java
@@ -1,85 +1,37 @@
 package lekt31_venstremenu;
 
 import android.os.Bundle;
-import com.google.android.material.navigation.NavigationView;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import dk.nordfalk.android.elementer.R;
 
-/**
- * BEMÆRK du skal have følgende i app/build.gradle før at eksemplet kan oversætte
- dependencies {
-   // Understøttelse af venstremenu
-   compile 'com.android.support:design:22.2.1'
- }
- */
-public class BenytVenstremenu extends AppCompatActivity implements OnClickListener {
-  // Vi erklærer variabler herude så de huskes fra metode til metode
-  Button knap1, knap2, knap3;
-  private DrawerLayout mDrawerLayout;
+
+public class BenytVenstremenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+  private DrawerLayout drawerLayout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.lekt31_benyt_venstremenu);
-    // findViewById() kan først kaldes efter setContentView()
-    knap1 = findViewById(R.id.knap1);
-    knap2 = findViewById(R.id.knap2);
-    knap3 = findViewById(R.id.knap3);
 
-    knap1.setOnClickListener(this);
-    knap2.setOnClickListener(this);
-    knap3.setOnClickListener(this);
-
-    mDrawerLayout = findViewById(R.id.drawer_layout);
+    drawerLayout = findViewById(R.id.drawer_layout);
     NavigationView navigationView = findViewById(R.id.nav_view);
-    if (navigationView != null) {
-      setupDrawerContent(navigationView);
-    }
-  }
-
-  private void setupDrawerContent(NavigationView navigationView) {
-    navigationView.setNavigationItemSelectedListener(
-            new NavigationView.OnNavigationItemSelectedListener() {
-              @Override
-              public boolean onNavigationItemSelected(MenuItem menuItem) {
-                menuItem.setChecked(true);
-                mDrawerLayout.closeDrawers();
-                return true;
-              }
-            });
+    navigationView.setNavigationItemSelectedListener(this);
+    drawerLayout.openDrawer(navigationView);
   }
 
 
-  public void onClick(View v) {
-    System.out.println("Der blev trykket på en knap");
-
-    // Vis et tal der skifter så vi kan se hver gang der trykkes
-    long etTal = System.currentTimeMillis();
-
-    if (v == knap1) {
-
-      knap1.setText("Du trykkede på mig. Tak! \n" + etTal);
-
-    } else if (v == knap2) {
-
-      knap3.setText("Nej nej, tryk på mig i stedet!\n" + etTal);
-
-    } else if (v == knap3) {
-
-      knap2.setText("Hey, hvis der skal trykkes, så er det på MIG!\n" + etTal);
-      // Erstat logoet med en bil
-      ImageView ikon = findViewById(R.id.ikon);
-      ikon.setImageResource(R.drawable.bil);
-
-    }
-
+  @Override
+  public boolean onNavigationItemSelected(MenuItem menuItem) {
+    menuItem.setChecked(true);
+    drawerLayout.closeDrawers();
+    Snackbar.make(drawerLayout, "Du trykkede på "+menuItem.toString(), Snackbar.LENGTH_SHORT).show();
+    return true;
   }
 }
