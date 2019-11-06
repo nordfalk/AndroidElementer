b044d041c4b2195a0de470b96b20de1f406607b5
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2019-11-06 13:36:04 +0100

    Forskellige forbedringer

---
+++ app/src/main/java/lekt05_grafik/Grafikdemo3.java
@@ -0,0 +1,98 @@
+package lekt05_grafik;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+
+/**
+ * @author Jacob Nordfalk
+ */
+public class Grafikdemo3 extends AppCompatActivity implements OnClickListener {
+
+  String teksten = "GrafikView";
+  Button okKnap;
+  EditText editText;
+  View minGrafik;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Opret grafik-view
+    minGrafik = new View(this) {
+
+      @Override
+      protected void onDraw(Canvas c) {
+        Paint tekstStregtype = new Paint();
+        tekstStregtype.setColor(Color.GREEN);
+        tekstStregtype.setTextSize(24);
+        c.rotate(15, 0, 0); // getWidth()/2, getHeight()/2// rotér 23 grader om midten
+        c.drawText(teksten, 0, 20, tekstStregtype);
+      }
+    };
+
+    TableLayout tableLayout = new TableLayout(this);
+
+    tableLayout.addView(minGrafik);
+    minGrafik.getLayoutParams().height = 60;
+
+    editText = new EditText(this);
+    editText.setText("Skriv noget tekst her");
+    editText.setSingleLine(true);
+
+    tableLayout.addView(editText);
+
+    okKnap = new ButtonPaaHovedet(this);
+    okKnap.setText("OK");
+    tableLayout.addView(okKnap);
+
+
+    ButtonPaaHovedet annullerKnap = new ButtonPaaHovedet(this);
+    annullerKnap.setText("Annuller!");
+    tableLayout.addView(annullerKnap);
+
+
+    setContentView(tableLayout);
+
+    okKnap.setOnClickListener(this);
+    annullerKnap.setOnClickListener(this);
+  }
+
+  public void onClick(View hvadBlevDerKlikketPå) {
+    System.out.println("Der blev klikket på " + hvadBlevDerKlikketPå);
+    teksten = editText.getText().toString();
+    minGrafik.invalidate();
+  }
+
+  /**
+   * @author j
+   */
+  public static class ButtonPaaHovedet extends AppCompatButton {
+
+    public ButtonPaaHovedet(Context c) {
+      super(c);
+    }
+
+    Paint p = new Paint();
+
+    @Override
+    protected void onDraw(Canvas c) {
+      c.save();
+      c.rotate(160, getWidth() / 2, getHeight() / 2);
+      c.scale(1f, 1.2f);
+      super.onDraw(c);
+      c.restore();
+      c.drawText("på hovedet", 5, getHeight() / 2, p);
+    }
+  }
+}
--- app/src/main/java/lekt05_grafik/KnapperMedEgenGrafik.java
+++ app/src/main/java/lekt05_grafik/KnapperMedEgenGrafik.java
@@ -30,35 +30,50 @@ public class KnapperMedEgenGrafik extends AppCompatActivity {
     super.onCreate(savedInstanceState);
 
     TableLayout tl = new TableLayout(this);
+    tl.setPadding(10,5,0,0);
 
     tl.addView(tv("Dette eksempel viser forskellige muligheder for knapper med egen grafik.\n"));
+    Button button1, button2;
 
-    tl.addView(tv("Alle views kan man sætte til at vise en anden baggrund - med knap så heldige resultater:"));
-    tl.addView(tr(tv("Normal"), tv("Egen baggrund")));
-    Button button1 = new Button(this);
-    Button button2 = new Button(this);
-    button2.setBackgroundResource(R.drawable.logo);
+    tl.addView(tv("Alle views kan man sætte til at vise en anden baggrund:"));
+    tl.addView(tr(tv("Her er en normal knap  "), tv("Her en knap med egen baggrund")));
+    button1 = new Button(this);
+    button2 = new Button(this);
+    button2.setBackgroundResource(R.drawable.bil);
     button1.setText("hejsa");
     button2.setText("hejsa");
     tl.addView(tr(button1, button2));
+    tl.addView(tv("MEN brugeren kan ikke se når de trykkes ned (feedback mangler - knappen skifter ikke farve)!\n"));
 
-    tl.addView(tv("\nBedre er at tilknytte grafik til venstre/højre/top eller bunden. Det understøtter Button (og faktisk også TextView)"));
-    tl.addView(tr(tv("Logo venstre"), tv("Logo top")));
+    tl.addView(tv("\nDu kan i stedet knytte grafikken til venstre/højre/top eller bunden af en knap:"));
+    tl.addView(tr(tv("Bil i top"), tv("Bil til venstre (uden tekst)")));
     tl.addView(tr(button1 = new Button(this), button2 = new Button(this)));
-    button1.setCompoundDrawablesWithIntrinsicBounds(R.drawable.logo,0,0,0);
-    button2.setCompoundDrawablesWithIntrinsicBounds(0,R.drawable.bil,0,0);
+    button1.setCompoundDrawablesWithIntrinsicBounds(0,R.drawable.bil,0,0);
+    button2.setCompoundDrawablesWithIntrinsicBounds(R.drawable.bil,0,0,0);
     button1.setText("hejsa");
-    button2.setText("hejsa");
+    button2.setText("");
 
-
-    tl.addView(tv("\n\nVil man ændre på billedets farve er den 'rigtige' løsning at lave knapgrafik til alle tilstande: normal, fokus, nedtrykket (og evt disablet) og kombinationerne, og så kombinere disse i en 'selector' drawable.\n"));
-    tl.addView(tr(tv("Blå knap"), tv("... disablet")));
+    tl.addView(tv("\n\nHvis du selv vil designe knappen er den rigtige løsning at lave en xml baggrund drawable med en 'ripple' effekt fra API v21 " +
+            "(og tidligere at lave knapgrafik til normal og nedtrykket og så kombinere disse i en 'selector' drawable)\n"));
+    tl.addView(tr(tv("Knap med selector"), tv("... disablet")));
     tl.addView(tr(button1 = new Button(this), button2 = new Button(this)));
-    button1.setBackgroundResource(R.drawable.knap_blaa_bg);
-    button2.setBackgroundResource(R.drawable.knap_blaa_bg);
+    button1.setBackgroundResource(R.drawable.knapbg_bil);
+    button2.setBackgroundResource(R.drawable.knapbg_bil);
     button2.setEnabled(false);
     button1.setText("hejsa");
     button2.setText("hejsa");
+    //  alle tilstande: normal, fokus, nedtrykket (og evt disablet) og kombinationerne,
+
+    tl.addView(tv("\n\nFor ensfarvede knapper kan du bruge 'shape'.\n"));
+    tl.addView(tr(tv("Blå knap"), tv("... disablet")));
+    tl.addView(tr(button1 = new Button(this), button2 = new Button(this)));
+    button1.setBackgroundResource(R.drawable.knapbg_blaa);
+    button2.setBackgroundResource(R.drawable.knapbg_blaa);
+    button2.setEnabled(false);
+    button1.setText("hejsa");
+    button2.setText("hejsa");
+
+
 
     tl.addView(tv("\nSe nogle eksempler (fra Android) på 'selector' drawables herunder.\nBemærk at de skifter farve når de trykkes ned:"));
     tl.addView(tr(tv("btn_star"), tv("btn_radio")));
@@ -118,10 +133,10 @@ public class KnapperMedEgenGrafik extends AppCompatActivity {
     tl.addView(tr(tv("ImageButton"), tv("Med farvefilter")));
 
     ImageButton imageButton1 = new ImageButton(this);
-    imageButton1.setImageResource(R.drawable.logo);
+    imageButton1.setImageResource(R.drawable.bil);
 
     ImageButton imageButton2 = new ImageButton(this);
-    imageButton2.setImageResource(R.drawable.logo);
+    imageButton2.setImageResource(R.drawable.bil);
     imageButton2.setOnTouchListener(farvKnapNårDenErTrykketNed);
     imageButton2.setOnFocusChangeListener(farvKnapNårFokus);
 
@@ -131,11 +146,11 @@ public class KnapperMedEgenGrafik extends AppCompatActivity {
     tl.addView(tr(tv("IB uden baggrund"), tv("Med farvefilter")));
 
     imageButton1 = new ImageButton(this);
-    imageButton1.setImageResource(R.drawable.logo);
+    imageButton1.setImageResource(R.drawable.bil);
     imageButton1.setBackgroundDrawable(null);
 
     imageButton2 = new ImageButton(this);
-    imageButton2.setImageResource(R.drawable.logo);
+    imageButton2.setImageResource(R.drawable.bil);
     imageButton2.setBackgroundDrawable(null);
     imageButton2.setOnTouchListener(farvKnapNårDenErTrykketNed);
 
@@ -145,10 +160,10 @@ public class KnapperMedEgenGrafik extends AppCompatActivity {
     tl.addView(tr(tv("ImageView"), tv("Med farvefilter")));
 
     ImageView knap5 = new ImageView(this);
-    knap5.setImageResource(R.drawable.logo);
+    knap5.setImageResource(R.drawable.bil);
 
     ImageView knap6 = new ImageView(this);
-    knap6.setImageResource(R.drawable.logo);
+    knap6.setImageResource(R.drawable.bil);
     knap6.setOnTouchListener(farvKnapNårDenErTrykketNed);
 
     tl.addView(tr(knap5, knap6));
