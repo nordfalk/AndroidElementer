38db7b8d94e975001e29e1ab5454cf445300150e
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2019-11-06 16:08:39 +0100

    Android Unit test eksempel

---
+++ app/src/androidTest/java/lekt01_views/BenytGalgespilTest.java
@@ -0,0 +1,71 @@
+package lekt01_views;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.rule.ActivityTestRule;
+import dk.nordfalk.android.elementer.R;
+import lekt02_aktiviteter.Hovedmenu_akt;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class BenytGalgespilTest {
+
+  @Rule
+  public ActivityTestRule<Hovedmenu_akt> testRule = new ActivityTestRule<>(Hovedmenu_akt.class);
+
+  @Test
+  public void hovedmenuTest() throws InterruptedException {
+    ViewInteraction knap1 = onView(withId(R.id.knap1));
+    ViewInteraction knap2 = onView(withId(R.id.knap2));
+    ViewInteraction knap3 = onView(withId(R.id.knap3));
+
+    // Det er ikke så smart at bruge Thread.sleep() da det får testen til at køre langsomt.
+    // Se https://developer.android.com/training/testing/espresso/idling-resource for løsninger
+    boolean langsomt = true;
+    if (langsomt) Thread.sleep(1000);
+
+    knap1.perform(click());
+    if (langsomt) Thread.sleep(1000);
+
+    knap1.check(doesNotExist());
+    if (langsomt) Thread.sleep(1000);
+
+    Espresso.pressBack();
+    if (langsomt) Thread.sleep(1000);
+
+    knap2.check(matches(isDisplayed()));
+    knap2.perform(click());
+    if (langsomt) Thread.sleep(1000);
+
+    onView(withText("En underskærm")).perform(click());
+    if (langsomt) Thread.sleep(1000);
+
+    Espresso.pressBack();
+    Espresso.pressBack();
+    if (langsomt) Thread.sleep(1000);
+
+    knap3.perform(click());
+    if (langsomt) Thread.sleep(1000);
+/*
+    ViewInteraction editText = onView(withText("Skriv et bogstav her."));
+    editText.check(matches(isDisplayed()));
+    editText.perform(replaceText("f"), closeSoftKeyboard());
+    if (langsomt) Thread.sleep(1000);
+
+    onView(withText("Gæt")).perform(click());
+    if (langsomt) Thread.sleep(1000);
+*/
+
+    //Log.d("AFPRØVNING", "Aktiviteten har på knap3 teksten: "+akt.knap3.getText());
+  }
+}
---
+++ app/src/androidTest/java/lekt01_views/BenytKnapperDeklarativTest.java
@@ -0,0 +1,57 @@
+package lekt01_views;
+
+
+import android.util.Log;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.rule.ActivityTestRule;
+import dk.nordfalk.android.elementer.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSubstring;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+public class BenytKnapperDeklarativTest {
+
+  @Rule
+  public ActivityTestRule<BenytKnapperDeklarativ> testRule = new ActivityTestRule(BenytKnapperDeklarativ.class);
+
+  @Test
+  public void trykKnapperTest() throws InterruptedException {
+    ViewInteraction knap1 = onView(withId(R.id.knap1));
+    ViewInteraction knap2 = onView(withId(R.id.knap2));
+    ViewInteraction knap3 = onView(withId(R.id.knap3));
+
+    // Det er ikke så smart at bruge Thread.sleep() da det får testen til at køre langsomt.
+    // Se https://developer.android.com/training/testing/espresso/idling-resource for løsninger
+    boolean langsomt = false;
+    if (langsomt) Thread.sleep(1000);
+    knap1.check(matches(withText("En knap")));
+    knap2.check(matches(withText("En anden knap")));
+    knap3.check(matches(withText("En tredje knap")));
+
+    knap1.perform(click());
+    if (langsomt) Thread.sleep(1000);
+    knap1.check(matches(not(withText("En knap"))));
+    knap2.check(matches(withText("En anden knap")));
+    knap3.check(matches(withText("En tredje knap")));
+
+    knap3.perform(click());
+    if (langsomt) Thread.sleep(1000);
+    knap1.check(matches(not(withText("En knap"))));
+    knap2.check(matches(withSubstring("Hey, hvis der skal trykkes")));
+    knap3.check(matches(withText("En tredje knap")));
+
+    knap2.perform(click());
+
+    BenytKnapperDeklarativ akt = testRule.getActivity();
+    Log.d("AFPRØVNING", "Aktiviteten har på knap3 teksten: "+akt.knap3.getText());
+  }
+}
