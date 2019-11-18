d4e2da32907b1be366cda5ee8fa77fd756bc56b2
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2019-11-18 15:21:30 +0100

    Småting

---
+++ app/src/main/java/lekt03x_temaer/BenytKnapperDeklarativLys.java
@@ -0,0 +1,9 @@
+package lekt03x_temaer;
+
+import lekt01_views.BenytKnapperDeklarativ;
+import lekt01_views.BenytKnapperProgrammatisk;
+
+
+public class BenytKnapperDeklarativLys extends BenytKnapperDeklarativ {
+  // Temaet styres i AndroidManifest.xml - kig efter android:theme=...
+}
